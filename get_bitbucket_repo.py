6b7b01fc452c6b1843a1dfa9904d52b919a3cf36
washing <1014585392@qq.com>
2024-01-16 22:32:18 +0800

    continue crawl after break & print log (#1)

--- get_bitbucket_repo.py
+++ get_bitbucket_repo.py
@@ -2,25 +2,41 @@ import requests
 import os
 import subprocess
 import sys
+import time
 
-url = 'https://api.bitbucket.org/2.0/repositories'
-
+url_file = "clone_urls"
 destination_folder = sys.argv[1] if len(sys.argv) > 2 else "bitbucket_repo"
-response = requests.get(url) #, auth=(username, password))
+next_page = 'https://api.bitbucket.org/2.0/repositories'
 
-repos = response.json()['values']
-repo_urls  = [repo['links']["clone"][0]["href"] for repo in repos]
+if os.path.exists(url_file):
+    with open(url_file, 'r', encoding="utf-8")as r:
+        repo_urls = [i.strip() for i in r.readlines()]
+    print("repos list already exists, clone repos now")
 
-# Handle pagination if necessary
-next_page = response.json().get('next')
-while next_page:
-    response = requests.get(next_page) #, auth=(username, password))
-    repos = response.json()['values']
-    repo_urls += [repo['links']["clone"][0]["href"] for repo in repos]
-    next_page = response.json().get('next')
+else:
+    repo_urls = list()
+    err_count = 0
+    counter = 0
+    while next_page:
+        try:
+            time.sleep(2)
+            response = requests.get(next_page)  #, auth=(username, password))
+            repos = response.json()['values']
+            repo_urls += [repo['links']['clone'][0]['href'] for repo in repos]
+            next_page = response.json().get("next")
+            counter += 1
+            print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} crawl a new page, page count {counter}")
+            err_count = 0
+        except:
+            err_count += 1
+            if err_count >= 3: break
+    with open(url_file, 'w', encoding="utf-8")as w:
+        w.write("\n".join(repo_urls))
+
 
 # Clone each repository into the specified folder
 for url in repo_urls:
+    time.sleep(1)
     repo_name = url.split('/')[-1].replace('.git', '') # Extract repository name from URL
     org_name = url.split('/')[-2]
     clone_path = os.path.join(destination_folder, org_name, repo_name)
